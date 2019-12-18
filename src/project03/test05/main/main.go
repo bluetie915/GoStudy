b21b143aea03034743a3bd9ed70838531af8653b
bluetie915 <1402711057@qq.com>
2019-12-18 17:39:34 +0800

    小项目

--- src/project03/test05/main/main.go
+++ src/project03/test05/main/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
-	"project03/test05/utils"
+	 util "project03/test05/utils"
 )
+//包的使用
 func main()  {
-	var result = utils.Add(3,5)
+	var result = util.Add(3,5)
 	fmt.Println("result=",result)
+
+	//跨包必须大写，小写为私有
+	var flag int = util.Num
+	fmt.Println("flag=",flag)
 }
--- src/project03/test05/utils/utils.go
+++ src/project03/test05/utils/utils.go
@@ -1,5 +1,7 @@
 package utils
 
+var Num int = 100
+
 func Add(m int,n int) int {
 	return m + n
 }
